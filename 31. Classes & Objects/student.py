7f50f90dca5738d6a9e6ee42d7ecbd7fc1d7630a
Amir Hamza <iamibayat@gmail.com>
2020-06-05 00:21:06 +0600

    Add files via upload

---
+++ 31. Classes & Objects/student.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, name, stu_id, major, gpa, is_on_probation):
+        self.name = name
+        self.stu_id = stu_id
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
