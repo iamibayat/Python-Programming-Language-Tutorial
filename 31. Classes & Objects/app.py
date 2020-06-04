d580e6833ae0be9883984004836a25f459126f1e
Amir Hamza <iamibayat@gmail.com>
2020-06-05 00:20:19 +0600

    Create app.py

---
+++ 31. Classes & Objects/app.py
@@ -0,0 +1,14 @@
+from student import Student
+
+student1 = Student("Amir", 1631050, "CSE", 3.3, False)
+
+print(student1.stu_id)
+print(student1.is_on_probation)
+print("\n")
+
+
+student2 = Student("Hamza", 1631030, "Business", 2.2, True)
+
+print(student2.name)
+print(student2.gpa)
+print(student2.is_on_probation)
