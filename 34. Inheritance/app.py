3bb3b71766be1d3dc3ec44e6b3b9b4066b07e5f5
Amir Hamza <iamibayat@gmail.com>
2020-06-05 00:25:00 +0600

    Create app.py

---
+++ 34. Inheritance/app.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+chef1 = Chef()
+chef1.makes_chicken()
+chef1.makes_specialdish()
+
+Chinesechef1 = ChineseChef()
+Chinesechef1.makes_chicken()
+Chinesechef1.makes_specialdish()
