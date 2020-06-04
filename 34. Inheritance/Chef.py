b8c1134964305b56e39fde130dc60b0762850c12
Amir Hamza <iamibayat@gmail.com>
2020-06-05 00:25:20 +0600

    Add files via upload

---
+++ 34. Inheritance/Chef.py
@@ -0,0 +1,10 @@
+class Chef:
+
+    def makes_chicken(self):
+        print("The chef makes chicken.")
+
+    def makes_salad(self):
+        print("The chef makes salad.")
+
+    def makes_specialdish(self):
+        print("The chef makes bbq ribs.")
---
+++ 34. Inheritance/ChineseChef.py
@@ -0,0 +1,10 @@
+from Chef import Chef
+
+
+class ChineseChef(Chef):
+
+    def makes_specialdish(self):
+        print("The chef makes pasta.")
+
+    def makes_friedrice(self):
+        print("This ChineseChef makes fried rice.")
