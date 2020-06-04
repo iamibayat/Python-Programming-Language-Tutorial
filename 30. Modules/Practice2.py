60fa1ef2891d891a6fdab7698cfb64668a85af39
Amir Hamza <iamibayat@gmail.com>
2020-06-05 00:18:44 +0600

    Add files via upload

---
+++ 30. Modules/Practice2.py
@@ -0,0 +1,24 @@
+def addition(num1, num2):
+    result = float(num1) + float(num2)
+    return result
+
+
+def subtraction(num1, num2):
+    result = float(num1) - float(num2)
+    return result
+
+
+def multiplication(num1, num2):
+    result = float(num1) * float(num2)
+    return result
+
+
+def division(num1, num2):
+    result = float(num1) / float(num2)
+    return result
+
+
+# print(addition(3, 7))
+# print(subtraction(5, 10))
+# print(multiplication(3.5, 4))
+# print(division(4.4, 2))
